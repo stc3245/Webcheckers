5a447dea534738b7e68684434f2d5dce5603b2c3
xxd9704 <xxd9704@g.rit.edu>
2018-10-04 09:03:15 -0400

    empty files to be implemented

---
+++ src/main/java/com/webcheckers/authModel/SignIn.java
@@ -0,0 +1,9 @@
+package com.webcheckers.authModel;
+
+/**
+ * The UI Model to GET the Signin page.
+ *
+ * @author <a href='mailto:xxd9704@rit.edu'>Perry Deng</a>
+ */
+public class SignIn {
+}
---
+++ src/main/java/com/webcheckers/ui/GetSigninRoute.java
@@ -0,0 +1,17 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ * The UI Controller to GET the signin page.
+ *
+ * @author <a href='mailto: xxd9704@rit.edu'>Perry Deng</a>
+ */
+public class GetSigninRoute implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
