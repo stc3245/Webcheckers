63d899efe1f8ac4376387f3a75f38820f918630e
stc3245 <stc3245@rit.edu>
2018-10-10 19:19:28 -0400

    Implemented Player and Message classes and added enum for message type.

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,4 +1,23 @@
 package com.webcheckers.model;
 
+/**
+ * Represents a player within the webcheckers game
+ *
+ * @author Sean Coyne 10-10-18
+ */
+
 public class Player {
+
+    //Private instance variable
+    private String name;
+
+    /**
+     * Gets the name of the player
+     *
+     * @return String - name
+     */
+    public String getName(){
+        return name;
+    }
+
 }
