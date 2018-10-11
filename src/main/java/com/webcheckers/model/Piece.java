325302686d6c1682cbaf6fb942a1cefcd95eec60
stc3245 <stc3245@rit.edu>
2018-10-10 20:13:49 -0400

    Added Space, Piece, PieceEnum classes. Implemented Piece class.

---
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -0,0 +1,32 @@
+package com.webcheckers.model;
+
+/**
+ * Represents a piece within the webcheckers game
+ *
+ * @author Sean Coyne 10-10-18
+ */
+
+public class Piece {
+
+    //Private instance variables
+    private PieceEnum type;
+    private ColorEnum color;
+
+    /**
+     * Get the type of this piece.
+     *
+     * @return PieceEnum - type of piece
+     */
+    public PieceEnum getType(){
+        return type;
+    }
+
+    /**
+     * Get the color of this piece.
+     *
+     * @return ColorEnum - color of piece
+     */
+    public ColorEnum getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/com/webcheckers/model/PieceEnum.java
@@ -0,0 +1,12 @@
+package com.webcheckers.model;
+
+/**
+ * Enum for storing different types of checkers pieces
+ *
+ * @author Sean Coyne 10-10-18
+ */
+
+public enum PieceEnum {
+    SINGLE,
+    KING
+}
