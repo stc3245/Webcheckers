ed18bf221c253224e17726916811326608f9d130
jrtechs <jxr8142@rit.edu>
2018-10-14 14:34:01 -0400

    Created the board view class which is an arraylist which is iterable.

--- src/main/java/com/webcheckers/model/BoardView.java
+++ src/main/java/com/webcheckers/model/BoardView.java
@@ -1,7 +1,36 @@
 package com.webcheckers.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class BoardView extends ArrayList<Row> {
 
+/**
+ * @author Jeffery Russell 10-14-18
+ */
+public class BoardView extends ArrayList<Row>
+{
+    /**
+     * Creates a new Board
+     */
+    public BoardView()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            this.add(new Row(i));
+        }
+    }
+
+
+    /**
+     * returns the inverted display for the black player.
+     */
+    public List<Row> getInverted()
+    {
+        ArrayList<Row> temp = new ArrayList;
+        for(int i = 7; i <= 0; i--)
+        {
+            temp.add(this.get(i));
+        }
+        return temp;
+    }
 }
