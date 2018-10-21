86205eb759e3a0fa226c97f1233f5a5a45ace539
stc3245 <stc3245@rit.edu>
2018-10-21 10:57:31 -0400

    Implemented basic Position and Move classes

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -1,5 +1,34 @@
 package com.webcheckers.model;
 
-public class Move
-{
+/**
+ * Represents a move within the webcheckers game
+ *
+ * @author Sean Coyne 10-21-18
+ */
+
+
+public class Move {
+
+    //Private instance variables
+    private Position startPosition;
+    private Position endPosition;
+
+    /**
+     * gets the start position of the move
+     *
+     * @return Position - start
+     */
+    public Position getStart(){
+        return startPosition;
+    }
+
+    /**
+     * gets the end position of the move
+     *
+     * @return Position - end
+     */
+    public Position getEndPosition(){
+        return endPosition;
+    }
+
 }
--- src/main/java/com/webcheckers/model/Position.java
+++ src/main/java/com/webcheckers/model/Position.java
@@ -1,5 +1,33 @@
 package com.webcheckers.model;
 
-public class Position
-{
+/**
+ * Represents a position within the webcheckers game
+ *
+ * @author Sean Coyne 10-21-18
+ */
+
+public class Position {
+
+    //Private instance variables
+    private int row;
+    private int cell;
+
+    /**
+     * gets the row of the position
+     *
+     * @return int - row number
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * gets the cell of the position
+     *
+     * @return int - cell number
+     */
+    public int getCell() {
+        return cell;
+    }
+
 }
